5fe6fb85799a73429eebe7ed4dda95fb5cbaf15a
Ala Ben Yahia <alabinyahia@gmail.com>
2024-02-01 14:38:51 +0100

    Add registration functionaliy and show success or error message accordingly

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {NzFormControlComponent, NzFormDirective, NzFormItemComponent, NzFormLabelComponent} from "ng-zorro-antd/form";
 import {NzInputDirective, NzInputGroupComponent} from "ng-zorro-antd/input";
 import {NzColDirective, NzRowDirective} from "ng-zorro-antd/grid";
 import {FormControl, FormGroup, NonNullableFormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
 import {NzButtonComponent} from "ng-zorro-antd/button";
-import {RouterLink} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
+import {AuthService} from "../../services/auth/auth.service";
+import {NzMessageService} from "ng-zorro-antd/message";
 
 @Component({
   selector: 'app-register',
@@ -26,6 +28,11 @@ import {RouterLink} from "@angular/router";
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  constructor(private fb: NonNullableFormBuilder,
+              private authService: AuthService,
+              private message: NzMessageService,
+              private router: Router) {}
+
   registerForm: FormGroup<{
     name: FormControl<string>;
     email: FormControl<string>;
@@ -36,20 +43,27 @@ export class RegisterComponent {
     email: ['', [Validators.email, Validators.required]],
   });
 
-  submitForm(): void {
+  register(): void {
     if (this.registerForm.valid) {
       console.log('submit', this.registerForm.value);
+      this.authService.register(this.registerForm.value).subscribe(res => {
+        console.log(res);
+        if (res.id != null) {
+          this.message.success("Registration successful !", {nzDuration: 3500});
+          this.router.navigateByUrl("/login");
+        } else {
+          this.message.error("Something went wrong!", {nzDuration: 3500});
+        }
+      })
     } else {
       Object.values(this.registerForm.controls).forEach(control => {
         if (control.invalid) {
           control.markAsDirty();
-          control.updateValueAndValidity({ onlySelf: true });
+          control.updateValueAndValidity({onlySelf: true});
         }
       });
     }
   }
-  constructor(private fb: NonNullableFormBuilder) {}
-
 
 
 }
--- src/app/auth/services/auth/auth.service.ts
+++ src/app/auth/services/auth/auth.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
+
+const BASE_URL = "http://localhost:8080"
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  register(registerRequest: any): Observable<any> {
+    return this.http.post(BASE_URL+'/api/auth/register', registerRequest);
+  }
 }
