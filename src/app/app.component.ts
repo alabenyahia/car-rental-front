84f89a1b79dc813126e0a5dc9413378d5d820d7a
Ala Ben Yahia <alabinyahia@gmail.com>
2024-02-03 12:00:36 +0100

    Show navbar accordingly to logged in user based on its role and add logout functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,37 @@
 import { Component } from '@angular/core';
-import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {Router, RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {NzHeaderComponent} from "ng-zorro-antd/layout";
 import {NzRowDirective} from "ng-zorro-antd/grid";
 import {NzButtonComponent} from "ng-zorro-antd/button";
+import {StorageService} from "./auth/services/storage/storage.service";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NzHeaderComponent, NzRowDirective, RouterLink, NzButtonComponent, RouterLinkActive],
+  imports: [RouterOutlet, NzHeaderComponent, NzRowDirective, RouterLink, NzButtonComponent, RouterLinkActive, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'car-rental-front';
+
+  isCustomerLoggedIn: boolean = StorageService.isCustomerLoggedIn();
+  isAdminLoggedIn: boolean = StorageService.isAdminLoggedIn();
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event.constructor.name === "NavigationEnd") {
+        this.isAdminLoggedIn = StorageService.isAdminLoggedIn();
+        this.isCustomerLoggedIn = StorageService.isCustomerLoggedIn();
+      }
+    })
+  }
+
+  logout() {
+    StorageService.logout();
+    this.router.navigateByUrl("/login");
+  }
 }
